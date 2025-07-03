2c0db2a24b6c1ec70496f41465d1bca8afe7bd4e
Muhammad Zaid <muhammadzaid00912@gmail.com>
2025-07-03 13:22:09 +0500

    deploy-config

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/lxzan/gws"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // fallback for local
+	}
+	fmt.Println("PORT is ", port)
+
 	server := gws.NewServer(&WSHandler{
 		RoomMgr:        NewRoomManager(),
 		PlayerIDToIP:   make(map[string]string),
 		ConnToPlayerID: make(map[*gws.Conn]string),
 	}, nil)
 
-	server.Run(":4040")
+	log.Fatal(server.Run(":" + port))
 }
